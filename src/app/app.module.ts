34b67416d6d402bbea7ee657b1dcaf8c205a4889
unknown <loick.paccaud@gmail.com>
2018-01-12 15:44:00 +0100

    debut API event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
 import { TestPage } from '../pages/test/test';
 import { RiflesProvider } from '../providers/rifles/rifles';
+import { HomeProvider } from '../providers/home/home';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { RiflesProvider } from '../providers/rifles/rifles';
     TestPage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -30,7 +33,8 @@ import { RiflesProvider } from '../providers/rifles/rifles';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    RiflesProvider
+    RiflesProvider,
+    HomeProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,20 +3,29 @@ import { NavController } from 'ionic-angular';
 
 import { TestPage } from '../test/test';
 
+import { HomeProvider} from '../../providers/home/home';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  worldState: any;
+  events: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public homeProvider: HomeProvider) {
+    this.getWorldState();
+  }
 
-
+  getWorldState() {
+    this.homeProvider.getworldState()
+    .then(data => {
+      this.worldState = data;
+      this.events = this.worldState.Events;
+    });
   }
 
   openTest() {
   	this.navCtrl.push(TestPage)
   }
-
-
 }
