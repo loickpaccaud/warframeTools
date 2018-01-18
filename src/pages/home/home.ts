3693896b9d6e8050b575e8f1c406bb3cb223b7ec
Menros <gondras.plouis@gmail.com>
2018-01-18 17:50:18 +0100

    improve homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,60 +14,34 @@ import { HomeProvider} from '../../providers/home/home';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  worldState: any;
-  events: any;
   items;
 
   constructor(public navCtrl: NavController, public homeProvider: HomeProvider) {
     this.items = [
       {
-        'title': 'Events',
-        'page': 'EventPage'
+        title: 'Events',
+        page: EventPage
       },
       {
-        'title': 'Alerts',
-        'page': 'AlertPage'
+        title: 'Alerts',
+        page: AlertPage
       },
       {
-        'title': 'Sorties',
-        'page': 'SortiePage'
+        title: 'Sorties',
+        page: SortiePage
       },
       {
-        'title': 'Invasions',
-        'page': 'InvasionPage'
+        title: 'Invasions',
+        page: InvasionPage
       },
       {
-        'title': 'Void trader',
-        'page': 'VoidTraderPage'
+        title: 'Void trader',
+        page: VoidTraderPage
       }
     ];
   }
 
-  openNavDetailsPage(page:string) {
-    switch (page) {
-      case "EventPage":
-        this.navCtrl.push(EventPage);
-        break;
-
-      case "AlertPage":
-        this.navCtrl.push(AlertPage);
-        break;
-
-      case "SortiePage":
-        this.navCtrl.push(SortiePage);
-        break;
-
-      case "InvasionPage":
-        this.navCtrl.push(InvasionPage);
-        break;
-
-      case "VoidTraderPage":
-        this.navCtrl.push(VoidTraderPage);
-        break;
-
-      default:
-        this.navCtrl.push(HomePage);
-        break;
-    }
+  openNavDetailsPage(page) {
+    this.navCtrl.push(page);
   }
 }
